3b4aa5bf25726681f8bcd72bc657021cdb1d01e8
Pablo Vahanian <pablo.vahanian@gmail.com>
2025-01-18 23:38:41 -0800

    Navbar fix

--- portfolionextjs/app/components/Navbar.tsx
+++ portfolionextjs/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const Navbar: React.FC = () => {
   return (
-    <div className="nav-bar">
+    <div className="nav-bar" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', padding: '10px 20px' }}>
       <Link href="/" passHref>
         <img
           className="logo"
@@ -13,12 +13,13 @@ const Navbar: React.FC = () => {
           style={{ cursor: 'pointer', width: '300px' }}
         />
       </Link>
-      <div className="nav-icons" style={{ display: 'flex', gap: '10px' }}>
+      <div className="nav-icons" style={{ display: 'flex', gap: '20px' }}>
         <a
           title="Email Me"
           href="mailto:pablo.vahanian@gmail.com"
           target="_blank"
           rel="noreferrer"
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', backgroundColor: '#e8f5fe', width: '60px', height: '60px' }}
         >
           <img src="/assets/Mail.svg" alt="Mail Icon" style={{ width: '55px', height: '55px' }} />
         </a>
@@ -27,6 +28,7 @@ const Navbar: React.FC = () => {
           href="https://www.linkedin.com/in/pablo-vahanian/"
           target="_blank"
           rel="noreferrer"
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', backgroundColor: '#e8f5fe', width: '60px', height: '60px' }}
         >
           <img src="/assets/Linkedin.svg" alt="LinkedIn Icon" style={{ width: '55px', height: '55px' }} />
         </a>
@@ -34,6 +36,7 @@ const Navbar: React.FC = () => {
           title="Resume"
           href="/PVResume.pdf"
           download
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', backgroundColor: '#e8f5fe', width: '60px', height: '60px' }}
         >
           <img src="/assets/Resume.png" alt="Resume Icon" style={{ width: '55px', height: '55px' }} />
         </a>
