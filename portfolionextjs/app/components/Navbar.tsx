1e45ac233e31e3c408a6c0cf91d1c77d66844924
Pablo Vahanian <pablo.vahanian@gmail.com>
2025-01-18 23:47:52 -0800

    navbar changed to Image instead of img

--- portfolionextjs/app/components/Navbar.tsx
+++ portfolionextjs/app/components/Navbar.tsx
@@ -1,17 +1,21 @@
 // app/components/Navbar.tsx
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar: React.FC = () => {
   return (
-    <div className="nav-bar" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', padding: '10px 20px' }}>
+    <div className="nav-bar" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-evenly'}}>
       <Link href="/" passHref>
-        <img
-          className="logo"
-          src="/assets/NewIcon2.svg"
-          alt="Pablo's Logo"
-          style={{ cursor: 'pointer', width: '300px' }}
-        />
+        <div style={{ cursor: 'pointer', width: '300px', height: 'auto' }}>
+          <Image
+            className="logo"
+            src="/assets/NewIcon2.svg"
+            alt="Pablo's Logo"
+            width={300}
+            height={75}
+          />
+        </div>
       </Link>
       <div className="nav-icons" style={{ display: 'flex', gap: '20px' }}>
         <a
@@ -19,26 +23,26 @@ const Navbar: React.FC = () => {
           href="mailto:pablo.vahanian@gmail.com"
           target="_blank"
           rel="noreferrer"
-          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', backgroundColor: '#e8f5fe', width: '60px', height: '60px' }}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', width: '60px', height: '60px' }}
         >
-          <img src="/assets/Mail.svg" alt="Mail Icon" style={{ width: '55px', height: '55px' }} />
+          <Image src="/assets/Mail.svg" alt="Mail Icon" width={55} height={55} />
         </a>
         <a
           title="LinkedIn Page"
           href="https://www.linkedin.com/in/pablo-vahanian/"
           target="_blank"
           rel="noreferrer"
-          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', backgroundColor: '#e8f5fe', width: '60px', height: '60px' }}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', width: '60px', height: '60px' }}
         >
-          <img src="/assets/Linkedin.svg" alt="LinkedIn Icon" style={{ width: '55px', height: '55px' }} />
+          <Image src="/assets/Linkedin.svg" alt="LinkedIn Icon" width={55} height={55} />
         </a>
         <a
           title="Resume"
           href="/PVResume.pdf"
           download
-          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', backgroundColor: '#e8f5fe', width: '60px', height: '60px' }}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', width: '60px', height: '60px' }}
         >
-          <img src="/assets/Resume.png" alt="Resume Icon" style={{ width: '55px', height: '55px' }} />
+          <Image src="/assets/Resume.png" alt="Resume Icon" width={55} height={55} />
         </a>
       </div>
     </div>
